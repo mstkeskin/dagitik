18b5df29e8d2d4ec3a289124adf0ceb0c24ff61b
GameOfCodes <mstkeskin@users.noreply.github.com>
2015-10-20 14:48:29 +0300

    Update odev2.py
    Odev3 ilk adım

--- odev2/odev2.py
+++ odev2/odev2.py
@@ -1,1 +1,19 @@
+import sys
 
+def main(argv):
+    if(len(sys.argv)!=2):
+        sys.exit("Lutfen 2den buyuk bir sayı giriniz ")
+
+    plaintext = list(raw_input("Lütfen bir mesaj giriniz"))
+    alfabe=list('abcdefghijklmnopqrstuvwxyz')
+    s = int(sys.argv[1])
+    sifreleme = ''
+
+    for c in plaintext:
+        if c in plaintext:
+            sifreleme += alfabe[(alfabe.index(c)+k)%(len(alfabe))]
+
+    print 'Şifrelenmiş mesajınız: ' + sifreleme
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
