bd72a9991c75b2be7c066b4e0334a28cf153f9b8
GameOfCodes <mstkeskin@users.noreply.github.com>
2015-10-20 15:30:36 +0300

    Update odev03.py
    Büyük harf ve boşluk karakteri yok sayıldı

--- odev03/odev03.py
+++ odev03/odev03.py
@@ -1,19 +1,25 @@
 import sys
 
+import threading
+
 def main(argv):
     if(len(sys.argv)!=2):
-        sys.exit("Lutfen 2den buyuk bir sayı giriniz ")
+        sys.exit("Lutfen 2den buyuk bir sayi giriniz ")
 
-    plaintext = list(raw_input("Lütfen bir mesaj giriniz"))
+    plaintext = list(raw_input("Lutfen bir mesaj giriniz"))
     alfabe=list('abcdefghijklmnopqrstuvwxyz')
     s = int(sys.argv[1])
     sifreleme = ''
 
     for c in plaintext:
-        if c in plaintext:
-            sifreleme += alfabe[(alfabe.index(c)+k)%(len(alfabe))]
+        if c in alfabe:
+            sifreleme += alfabe[(alfabe.index(c)-s)%(len(alfabe))]
+        else:
+            sifreleme +=c
 
-    print 'Şifrelenmiş mesajınız: ' + sifreleme
+    sifreleme=sifreleme.upper()
+
+    print 'Sifreli mesajiniz: ' + sifreleme
 
 if __name__ == "__main__":
     main(sys.argv[1:])
