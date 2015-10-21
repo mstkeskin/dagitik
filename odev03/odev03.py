dab00d4e781d41a6323d4e016ce786160c5df258
GameOfCodes <mstkeskin@users.noreply.github.com>
2015-10-21 20:30:40 +0300

    Update odev03.py
    Thread çalışıyor ama loop olmuyor.

--- odev03/odev03.py
+++ odev03/odev03.py
@@ -1,75 +1,92 @@
 __author__ = 'mesutkeskin'
 
-import sys
+import Queue
 import threading
 import time
 
+exitFlag = 0
+
 class myThread(threading.Thread):
-    def __init__(self,threadID,name):
+    def __init__(self,threadID,name,q):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+        self.q = q
+
 
 
     def run(self):
-        threadLock.acquire()
-        oku_ve_sifrele()
-        threadLock.release()
+        print "Starting " + self.name
+        #threadLock.acquire()
+        oku_ve_sifrele(self.name,self.q)
+        #threadLock.release()
+        print "Exiting " + self.name
 
 
 #def main(argv):
  #   if(len(sys.argv)!=2):
         #sys.exit("Lutfen 2den buyuk bir sayi giriniz ")
 
-kayma_sayisi=input("Lutfen kayma sayisi giriniz:")
+s=input("Lutfen kayma sayisi giriniz:")
 num_thread=input("Lutfen thread sayisini belirtiniz:")
 num_character=input("Lutfen calisilacak karakter sayisini belirtiniz:")
 global_file=open('metin.txt','r')
 
 
-def oku_ve_sifrele():
+def oku_ve_sifrele(threadName,q):
+   while not exitFlag:
+       queueLock.acquire()
+       if not workQueue.empty():
+            data = q.get()
+            plaintext=global_file.read(num_character)
+            plaintext=plaintext.lower()
+            alfabe=list('abcdefghijklmnopqrstuvwxyz')
+
+            sifreleme = ''
+            for c in plaintext:
+                if c in alfabe:
+                    sifreleme += alfabe[(alfabe.index(c)-s)%(len(alfabe))]
+                else:
+                    sifreleme +=c
+
+                sifreleme=sifreleme.upper()
+            filename="crypted_%d_%d_%dtxt" %(s,num_thread,num_character)
+            file=open(filename,'a')
+            file.write(sifreleme)
+            file.close()
+
+            queueLock.release()
+
+       else:
+           queueLock.release()
 
 
-    #plaintext = list(raw_input("Lutfen bir mesaj giriniz").lower())
-    plaintext=global_file.read(num_character)
-    plaintext=plaintext.lower()
-    alfabe=list('abcdefghijklmnopqrstuvwxyz')
-    #s = int(sys.argv[1])
-    sifreleme = ''
 
 
+queueLock = threading.Lock()
+workQueue = Queue.Queue()
 
-    for c in plaintext:
-        if c in alfabe:
-            sifreleme += alfabe[(alfabe.index(c)-kayma_sayisi)%(len(alfabe))]
-        else:
-            sifreleme +=c
-
-    sifreleme=sifreleme.upper()
-
-    filename="crypted_<%d>_<%d>_<%d>.txt" %(kayma_sayisi,num_thread,num_character)
-    file=open(filename,'a')
-    file.write(sifreleme)
-    file.close()
 
 threadID=0
-threadLock = threading.Lock()
+
+
 threads = []
 for n in range(num_thread):
-
-    thread =myThread(threadID,'thread_'+str(threadID))
+    thread =myThread(threadID,'thread_'+str(threadID),workQueue)
     thread.start()
     threads.append(thread)
-    threadID=threadID+1
+    threadID +=1
 
+queueLock.acquire()
+for l in range(num_thread):
+             workQueue.put(l)
+queueLock.release()
 
+while not workQueue.empty():
+    pass
 
-
-
-    #print 'Sifreli mesajiniz: ' + sifreleme
-
-#if __name__ == "__main__":
- #   main(sys.argv[1:])
+exitFlag = 1
 
 for t in threads:
-    t.join()
+     t.join()
+print "Exiting Main Thread"
